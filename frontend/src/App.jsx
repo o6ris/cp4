ff267f337f8d87b0992f2c5921199a6585df13ac
o6ris <loic.tsiry@gmail.com>
2023-02-03 16:20:00 +0100

    navigate to /Login if route dosen't match

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,10 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 import Login from "@pages/Login";
 import SearchCity from "@pages/SearchCity";
 
@@ -13,6 +18,7 @@ function App() {
         <Routes>
           <Route path="/Login" element={<Login />} />
           <Route path="/FindCity" element={<SearchCity />} />
+          <Route path="*" element={<Navigate replace to="/Login" />} />
         </Routes>
       </div>
     </Router>
