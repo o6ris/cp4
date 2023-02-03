4e7f92c619ad3b3e87106134464a841b43ee4cd3
o6ris <loic.tsiry@gmail.com>
2023-02-03 09:42:54 +0100

    inner join on findAllByCity to get users infos

--- backend/src/models/ReviewManager.js
+++ backend/src/models/ReviewManager.js
@@ -7,9 +7,10 @@ class ReviewManager extends AbstractManager {
 
   findAllByCity(id) {
     return this.connection.query(
-      `select id_city, id_user, date_post, arrival_date, return_date, security, activities, cost_of_living, environement, public_transportation, weather, shops, nightlife, comment, cities.name as cityName, cities.picture as cityPicture
+      `select id_city, id_user, date_post, arrival_date, return_date, security, activities, cost_of_living, environement, public_transportation, weather, shops, nightlife, comment, cities.name as cityName, cities.picture as cityPicture, users.alias as userAlias
       from  ${this.table}
       inner join cities on ${this.table}.id_city = cities.id
+      inner join users on ${this.table}.id_user = users.id
       where id_city = ?`,
       [id]
     );
