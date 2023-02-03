b160799889bc14484cc8fca739ce828dad0024d4
o6ris <loic.tsiry@gmail.com>
2023-02-03 09:31:51 +0100

    Create route back to get all reviews by one city

--- backend/src/router.js
+++ backend/src/router.js
@@ -4,10 +4,13 @@ const router = express.Router();
 
 const userControllers = require("./controllers/userControllers");
 const cityControllers = require("./controllers/cityControllers");
+const reviewControllers = require("./controllers/reviewControllers");
 const validators = require("./services/validators");
 
 router.post("/login", validators.checkUser, userControllers.validateUser);
 
 router.get("/cities", cityControllers.browse);
+
+router.get("/cityReviews/:id", reviewControllers.browse);
 
 module.exports = router;
