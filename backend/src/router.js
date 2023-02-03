f5e01944ae4ba5e924ce64d21af51cf68e3c1c69
o6ris <loic.tsiry@gmail.com>
2023-02-03 09:17:01 +0100

    create route back for get all cities

--- backend/src/router.js
+++ backend/src/router.js
@@ -3,8 +3,11 @@ const express = require("express");
 const router = express.Router();
 
 const userControllers = require("./controllers/userControllers");
+const cityControllers = require("./controllers/cityControllers");
 const validators = require("./services/validators");
 
 router.post("/login", validators.checkUser, userControllers.validateUser);
+
+router.get("/cities", cityControllers.browse);
 
 module.exports = router;
