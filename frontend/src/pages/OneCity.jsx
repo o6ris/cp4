b265a9d1ab2676701a65a89c6f9d0e71d42129df
o6ris <loic.tsiry@gmail.com>
2023-02-04 11:24:48 +0100

    display score on One city

--- frontend/src/pages/OneCity.jsx
+++ frontend/src/pages/OneCity.jsx
@@ -27,15 +27,21 @@ function OneCity() {
             style={{ backgroundImage: `url(${city[0]?.cityPicture})` }}
           />
           <div className="bg-black opacity-40 w-full h-72 absolute" />
-          <div className="absolute flex flex-col h-72 justify-center items-center gap-3">
+          <div className="absolute flex flex-col h-72 justify-center items-center drop-shadow-lg gap-3">
+            <div className="bg-white opacity-80 p-5 rounded-md">
+              <h2 className="text-xl">{city[0]?.avgScore}</h2>
+            </div>
             <h1 className="text-2xl text-white text-center text-shadow">
-              Bienvenu à {city[0]?.cityName}
+              Bienvenue à {city[0]?.cityName}
             </h1>
             <ButtonTemplate
               buttonType="submit"
               buttonText="Post your Review!"
               buttonStyle="buttonStyle"
             />
+          </div>
+          <div className="grid grid-cols-2 gap-4">
+            <div>Bonjour</div>
           </div>
         </>
       )}
